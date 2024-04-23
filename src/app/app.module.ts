19e6d639ddfd417b4f2153e4ab8a79cbc66fc2dc
Peter Guilhamet <peterguilhametjr@gmail.com>
2024-04-23 15:18:26 -0700

    added buttons to take you to user pages, driver pages, and owner pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './search/search.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
+import { HeaderComponent } from './user/header/header.component';
+import { SearchComponent } from './user/search/search.component';
+import { SearchBarComponent } from './user/search-bar/search-bar.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-import { CartComponent } from './cart/cart.component';
-import { OffersPageComponent } from './offers-page/offers-page.component';
-import { RestaurantPageComponent } from './restaurant-page/restaurant-page.component';
+import { CartComponent } from './user/cart/cart.component';
+import { OffersPageComponent } from './user/offers-page/offers-page.component';
+import { RestaurantPageComponent } from './user/restaurant-page/restaurant-page.component';
+import { OwnerPageComponent } from './owner/owner-page/owner-page.component';
+import { DriverPageComponent } from './driver/driver-page/driver-page.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { RestaurantPageComponent } from './restaurant-page/restaurant-page.compo
     HomeComponent,
     CartComponent,
     OffersPageComponent,
-    RestaurantPageComponent
+    RestaurantPageComponent,
+    OwnerPageComponent,
+    DriverPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/owner/owner-page/owner-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-page',
+  templateUrl: './owner-page.component.html',
+  styleUrl: './owner-page.component.css'
+})
+export class OwnerPageComponent {
+
+}
